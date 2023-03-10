04b1de306f72186008bb4830fceda0d019336651
AlehNat <natsevsky@gmail.com>
2023-03-10 18:28:01 +0100

    add tetu boosted pool initialization scripts

---
+++ scripts/tetuBoosted/InitializeDAILineraPool.ts
@@ -0,0 +1,100 @@
+import { ethers } from "hardhat"
+import LinearPoolABI from "../abis/LinearPool.json"
+import { MaticAddresses } from "../addresses/MaticAddresses"
+import { ERC4626Strict, TetuV1SingleTokenStrictStrategy } from "../../typechain"
+import BalancerVaultABI from "../abis/BalancerVault.json"
+
+const DAI_ADDRESS = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
+const DAI_LINEAR_POOL_ADDRESS = "0xDa1CD1711743e57Dd57102E9e61b75f3587703da"
+const DAI_4626_ADDRESS = "0xacE2aC58E1E5A7BFE274916c4d82914D490Ed4a5"
+
+const BALANCER_VAULT_ADDRESS = "0xBA12222222228d8Ba445958a75a0704d566BF2C8"
+
+async function main() {
+  const signer = (await ethers.getSigners())[0]
+  console.log(`signer address is ${signer.address}`)
+
+  const dai = await ethers.getContractAt("IERC20", DAI_ADDRESS)
+  console.log(`signer DAI balance is ${await dai.balanceOf(signer.address)}`)
+  const vault = new ethers.Contract(BALANCER_VAULT_ADDRESS, BalancerVaultABI, signer)
+
+  await dai.approve(vault.address, ethers.utils.parseUnits("100", 18))
+
+  const tDAIPool = await ethers.getContractAt("ERC4626Strict", DAI_4626_ADDRESS)
+  await dai.approve(tDAIPool.address, ethers.utils.parseUnits("100", 18))
+  await tDAIPool.deposit(ethers.utils.parseUnits("5", 18), signer.address)
+  console.log(`tDAI4626Strict balance is ${await tDAIPool.balanceOf(signer.address)}`)
+  await tDAIPool.approve(vault.address, ethers.utils.parseUnits("100", 18))
+
+  const daiLinerPool = new ethers.Contract(DAI_LINEAR_POOL_ADDRESS, LinearPoolABI, signer)
+  console.log("swap 1 (DAI -> bb-t-DAI) join pool")
+  await vault.swap(
+    {
+      poolId: await daiLinerPool.getPoolId(),
+      kind: 0, // GIVEN_IN
+      assetIn: DAI_ADDRESS,
+      assetOut: DAI_LINEAR_POOL_ADDRESS,
+      userData: "0x",
+      amount: ethers.utils.parseUnits("5", 18)
+    },
+    {
+      sender: signer.address,
+      fromInternalBalance: false,
+      toInternalBalance: false,
+      recipient: signer.address
+    },
+    1,
+    Date.now() + 1000
+  )
+
+  console.log(`Signer BPT balance: ${await daiLinerPool.balanceOf(signer.address)}`)
+  console.log(`Signer USDC balance: ${await dai.balanceOf(signer.address)}`)
+  console.log("swap 2 (tUSDC -> bb-t-USDC) join pool")
+  await vault.swap(
+    {
+      poolId: await daiLinerPool.getPoolId(),
+      kind: 0, // GIVEN_IN
+      assetIn: tDAIPool.address,
+      assetOut: DAI_LINEAR_POOL_ADDRESS,
+      userData: "0x",
+      amount: ethers.utils.parseUnits("5", 18)
+    },
+    {
+      sender: signer.address,
+      fromInternalBalance: false,
+      toInternalBalance: false,
+      recipient: signer.address
+    },
+    1,
+    Date.now() + 1000
+  )
+
+  console.log(`Signer BPT balance: ${await daiLinerPool.balanceOf(signer.address)}`)
+  console.log(`Signer USDC balance: ${await dai.balanceOf(signer.address)}`)
+
+  console.log(`Signer BPT balance: ${await daiLinerPool.balanceOf(signer.address)}`)
+  console.log(`Signer USDC balance: ${await dai.balanceOf(signer.address)}`)
+
+  const poolTokens = await vault.getPoolTokens(await daiLinerPool.getPoolId())
+  const t0 = await ethers.getContractAt("IERC20Metadata", poolTokens.tokens[0])
+  const t1 = await ethers.getContractAt("IERC20Metadata", poolTokens.tokens[1])
+  const t2 = await ethers.getContractAt("IERC20Metadata", poolTokens.tokens[2])
+  console.log(`t0: ${await t0.symbol()}`)
+  console.log(`t1: ${await t1.symbol()}`)
+  console.log(`t2: ${await t2.symbol()}`)
+
+  const poolId = await daiLinerPool.getPoolId()
+  console.log(`pool tokens: ${poolTokens} `)
+  console.log(`pool info t0: ${await vault.getPoolTokenInfo(poolId, t0.address)} `)
+  console.log(`pool info t1: ${await vault.getPoolTokenInfo(poolId, t1.address)} `)
+  console.log(`pool info t2: ${await vault.getPoolTokenInfo(poolId, t2.address)} `)
+
+  console.log("Done ")
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
---
+++ scripts/tetuBoosted/InitializeUSDComposablePool.ts
@@ -0,0 +1,74 @@
+import { ethers } from "hardhat"
+import BalancerVaultABI from "../abis/BalancerVault.json"
+import ComposableStablePoolABI from "../abis/ComposableStablePool.json"
+import { BigNumber } from "ethers"
+
+const USDT_LINEAR_POOL_ADDRESS = "0x7c82A23B4C48D796dee36A9cA215b641C6a8709d"
+const DAI_LINEAR_POOL_ADDRESS = "0xDa1CD1711743e57Dd57102E9e61b75f3587703da"
+const USDC_LINEAR_POOL_ADDRESS = "0xae646817e458C0bE890b81e8d880206710E3c44e"
+
+const BALANCER_VAULT_ADDRESS = "0xBA12222222228d8Ba445958a75a0704d566BF2C8"
+const USD_COMPOSABLE_POOL_ADDRESS = "0xb3d658d5b95BF04E2932370DD1FF976fe18dd66A"
+
+
+async function main() {
+  const signer = (await ethers.getSigners())[0]
+  console.log(`signer address is ${signer.address}`)
+
+  const bbtUSDC = await ethers.getContractAt("IERC20", USDC_LINEAR_POOL_ADDRESS)
+  const bbtUSDCBalance = await bbtUSDC.balanceOf(signer.address)
+  const bbtUSDT = await ethers.getContractAt("IERC20", USDT_LINEAR_POOL_ADDRESS)
+  const bbtUSDTBalance = await bbtUSDT.balanceOf(signer.address)
+  const bbtDAI = await ethers.getContractAt("IERC20", DAI_LINEAR_POOL_ADDRESS)
+  const bbtDAIBalance = await bbtDAI.balanceOf(signer.address)
+  const vault = new ethers.Contract(BALANCER_VAULT_ADDRESS, BalancerVaultABI, signer)
+
+  const usdComposablePool = new ethers.Contract(USD_COMPOSABLE_POOL_ADDRESS, ComposableStablePoolABI, signer);
+
+  const allTokens = [USDT_LINEAR_POOL_ADDRESS, USDC_LINEAR_POOL_ADDRESS, USD_COMPOSABLE_POOL_ADDRESS, DAI_LINEAR_POOL_ADDRESS];
+
+  const currentBalances = [bbtUSDTBalance, bbtUSDCBalance, BigNumber.from(0), bbtDAIBalance];
+  // not sure why we need to add "519229685853482762853049632900000000" for BPT token.
+  // Copied from the boosted aave pool initialization
+  const maxBalances = [bbtUSDTBalance, bbtUSDCBalance, BigNumber.from("519229685853482762853049632900000000"), bbtDAIBalance];
+  console.log("Current balances", currentBalances);
+
+
+  console.log("Approoving vault")
+  await bbtUSDC.approve(BALANCER_VAULT_ADDRESS, bbtUSDCBalance)
+  await bbtUSDT.approve(BALANCER_VAULT_ADDRESS, bbtUSDTBalance)
+  await bbtDAI.approve(BALANCER_VAULT_ADDRESS, bbtDAIBalance)
+  await usdComposablePool.approve(BALANCER_VAULT_ADDRESS, bbtDAIBalance)
+
+  const JOIN_KIND_INIT = 0
+  const initUserData = ethers.utils.defaultAbiCoder.encode(
+    ["uint256", "uint256[]"],
+    [JOIN_KIND_INIT, currentBalances]
+  )
+  const poolId = await usdComposablePool.getPoolId();
+  console.log("Pool id", poolId)
+  console.log("Join Init vault")
+  const tx = await vault.joinPool(
+    poolId,
+    signer.address,
+    signer.address,
+    {
+      assets: allTokens,
+      maxAmountsIn: maxBalances,
+      userData: initUserData,
+      fromInternalBalance: false,
+    })
+
+  console.log("Joining pool", tx.hash);
+  await tx.wait();
+  console.log("Balance after joining", await usdComposablePool.balanceOf(signer.address));
+
+  console.log("Done ")
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
