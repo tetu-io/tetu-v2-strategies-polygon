ebf6cddfa8bfc06850d2e22ddb1d25071d69295d
Alien Deployer <m@a17.name>
2023-03-15 17:06:55 +0300

    univ3: fix deploy script

--- deploy/016_UsdcUsdtUniswapV3Strategy.ts
+++ deploy/016_UsdcUsdtUniswapV3Strategy.ts
@@ -2,54 +2,17 @@ import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 import {ethers} from "hardhat";
 import {
-  ControllerV2,
-  ProxyControlled, TetuVaultV2,
-  UniswapV3ConverterStrategy,
-  VaultFactory
+  ProxyControlled,
+  UniswapV3ConverterStrategy
 } from "../typechain";
 import {Addresses} from "@tetu_io/tetu-contracts-v2/dist/scripts/addresses/addresses";
 import {CoreAddresses} from "@tetu_io/tetu-contracts-v2/dist/scripts/models/CoreAddresses";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre
-  const {deployer, USDC_ADDRESS, CONVERTER_ADDRESS, UNISWAPV3_USDC_USDT_100} = await getNamedAccounts()
-  if (await hre.getChainId() !== '137') {
-    console.error('Only matic or matic forking supported')
-    return
-  }
+  const {deployer, CONVERTER_ADDRESS, UNISWAPV3_USDC_USDT_100, SPLITTER_USDC_ADDRESS} = await getNamedAccounts()
 
   const core = Addresses.getCore() as CoreAddresses
-
-  const vaultName = 'TETUV2_USDC_USDT_UNISWAPV3'
-  const symbol = 'X_USDC_USDT_UNISWAPV3'
-  const vaultBuffer = 100
-
-  const controllerContract = await ethers.getContractAt("ControllerV2", core.controller) as ControllerV2
-  if (!await controllerContract.isOperator(deployer)) {
-    if(hre.network.name === "hardhat") {
-      const govSigner = await ethers.getImpersonatedSigner(await controllerContract.governance())
-      await controllerContract.connect(govSigner).registerOperator(deployer)
-    } else {
-      console.error('Deployer is not operator. Only operators can create vaults.')
-      return
-    }
-  }
-
-  const factoryContract = await ethers.getContractAt("VaultFactory", core.vaultFactory) as VaultFactory
-  await factoryContract.createVault(
-    USDC_ADDRESS,
-    vaultName,
-    symbol,
-    core.gauge,
-    vaultBuffer
-  )
-  const vaultIndex = (await factoryContract.deployedVaultsLength()).toNumber() - 1
-  const vaultAddress = await factoryContract.deployedVaults(vaultIndex)
-  console.log('Vault address:', vaultAddress)
-  const vaultContract = await ethers.getContractAt("TetuVaultV2", vaultAddress) as TetuVaultV2
-  await controllerContract.registerVault(vaultAddress)
-  const splitterAddress = await vaultContract.splitter()
-  console.log('Splitter address:', splitterAddress)
 
   const strategyImplDeployment = await deployments.get('UniswapV3ConverterStrategy')
   const proxyDeployResult = await deployments.deploy('UsdcUsdtUniswapV3Strategy', {
@@ -63,7 +26,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   await strategyContract.init(
     core.controller,
-    splitterAddress,
+    SPLITTER_USDC_ADDRESS,
     CONVERTER_ADDRESS,
     UNISWAPV3_USDC_USDT_100,
     0,
