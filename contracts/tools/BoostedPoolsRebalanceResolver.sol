161cadfced39765b61b3ceb7bdeb2e269d55529d
belbix <7453635@gmail.com>
2023-04-14 12:18:57 +0300

    fix bb rebalance resolver

--- contracts/tools/BoostedPoolsRebalanceResolver.sol
+++ contracts/tools/BoostedPoolsRebalanceResolver.sol
@@ -9,6 +9,8 @@ import "../integrations/balancer/ILinearPoolSimple.sol";
 import "../integrations/balancer/IBVault.sol";
 import "@tetu_io/tetu-contracts-v2/contracts/lib/StringLib.sol";
 
+//import "hardhat/console.sol";
+
 contract BoostedPoolsRebalanceResolver is OwnableUpgradeable {
   using SafeERC20 for IERC20;
 
@@ -17,7 +19,7 @@ contract BoostedPoolsRebalanceResolver is OwnableUpgradeable {
   ///////////////////////////////////////////////////
 
   uint public constant TETU_DENOMINATOR = 1000;
-  uint public constant DEFAULT_EXTRA_MAIN = 1000000;
+  uint public constant DEFAULT_EXTRA_MAIN = 5;
 
   ///////////////////////////////////////////////////
   //             VARIABLES
@@ -131,12 +133,28 @@ contract BoostedPoolsRebalanceResolver is OwnableUpgradeable {
       uint tetuLowerTarget = middleTarget - (upperTarget - middleTarget) * tetuNominator / TETU_DENOMINATOR;
       uint tetuUpperTarget = middleTarget + (upperTarget - middleTarget) * tetuNominator / TETU_DENOMINATOR;
 
+//      console.log('mainBalanceAdjusted', mainBalanceAdjusted / 1e18);
+//      console.log('wrappedBalanceAdjusted', wrappedBalanceAdjusted / 1e18);
+//      console.log('lowerTarget', lowerTarget / 1e18);
+//      console.log('upperTarget', upperTarget / 1e18);
+//      console.log('middleTarget', middleTarget / 1e18);
+//      console.log('tetuLowerTarget', tetuLowerTarget / 1e18);
+//      console.log('tetuUpperTarget', tetuUpperTarget / 1e18);
+
       if (mainBalanceAdjusted + wrappedBalanceAdjusted < middleTarget) {
         // not enough liquidity, skipping the pool
         continue;
       } else {
-        bool extra = mainBalanceAdjusted > tetuUpperTarget || mainBalanceAdjusted < tetuLowerTarget;
-        return (true, abi.encodeCall(BoostedPoolsRebalanceResolver.rebalance, (rebalancers[i], DEFAULT_EXTRA_MAIN, extra)));
+
+        // 29k > 50k || 29k < 0
+        if (mainBalanceAdjusted > upperTarget || mainBalanceAdjusted < lowerTarget) {
+          return (true, abi.encodeCall(BoostedPoolsRebalanceResolver.rebalance, (rebalancers[i], DEFAULT_EXTRA_MAIN * 10 ** mainDecimals, false)));
+        }
+
+        // 29k > 37.5k || 29k < 12.5k
+        if (mainBalanceAdjusted > tetuUpperTarget || mainBalanceAdjusted < tetuLowerTarget) {
+          return (true, abi.encodeCall(BoostedPoolsRebalanceResolver.rebalance, (rebalancers[i], DEFAULT_EXTRA_MAIN * 10 ** mainDecimals, true)));
+        }
       }
     }
     return (false, "all pools in range");
--- test/tools/BoostedPoolsRebalanceResolverTest.ts
+++ test/tools/BoostedPoolsRebalanceResolverTest.ts
@@ -1,45 +1,77 @@
-// import { ethers, upgrades } from 'hardhat'
-// import { BoostedPoolsRebalanceResolver } from '../../typechain'
-// import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
-// import { expect } from 'chai'
-// import { BigNumber } from 'ethers'
-//
-// describe('BoostedPoolsRebalanceResolverTest tests', function() {
-//
-//   const USDC_ADDRESS = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
-//   const USDC_BIG_HOLDER_ADDRESS = '0xe7804c37c13166ff0b37f5ae0bb07a3aebb6e245'
-//   const TETU_BOOSTED_USDC_REBALANCER = '0x9756549a334bd48423457d057e8edbfaf2104b16'
-//
-//   const DEFAULT_AMOUNT = '1000'
-//
-//   async function deployContracts() {
-//     const [signer, other] = await ethers.getSigners()
-//
-//     const BoostedPoolsRebalanceResolverFact = await ethers.getContractFactory('BoostedPoolsRebalanceResolver')
-//     const resolver = await upgrades.deployProxy(BoostedPoolsRebalanceResolverFact, [other.address, "300"]) as BoostedPoolsRebalanceResolver;
-//
-//     const balRebalancer = await ethers.getContractAt('ILinearPoolRebalancer', TETU_BOOSTED_USDC_REBALANCER);
-//     const poolAddress = await balRebalancer.getPool();
-//     const pool = await ethers.getContractAt("IBalancerBoostedAaveStablePool", poolAddress);
-//     const poolId = await pool.getPoolId();
-//     const vault = await ethers.getContractAt("IBVault", await pool.getVault());
-//
-//
-//     return { resolver, vault, poolId, signer }
-//   }
-//
-//   describe('Smoke tests', function() {
-//     it('Owner properly set', async function() {
-//       const { resolver, signer } = await loadFixture(deployContracts)
-//       expect(signer.address).is.eq(await resolver.owner())
-//     })
-//
-//     it('Checker smoke test', async function() {
-//       const { resolver, signer } = await loadFixture(deployContracts)
-//       await resolver.updateRebalancers([TETU_BOOSTED_USDC_REBALANCER])
-//       const {canExec} = await resolver.checker()
-//       expect(canExec).is.eq(true)
-//     })
-//
-//   })
-// })
+import { ethers, upgrades } from 'hardhat';
+import { BoostedPoolsRebalanceResolver, BoostedPoolsRebalanceResolver__factory } from '../../typechain';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+
+const rebalancers = [
+  '0x47Ada091aB72627AF6a7EAd768aD2e39e085A342', // DAI
+  '0x9756549A334Bd48423457D057e8EDbFAf2104b16', // USDC
+  '0xf30d0756053734128849666E01a0a4C04A5603C6', // USDT
+  '0x65c574A3e3ceae1CB8c9d46d92aE4b32F3f33D3c', // stMATIC
+  '0xC9c3bA34aBd888C7Bb68EA1d2f5650965b543Fbc', // MATIC
+]
+
+describe('BoostedPoolsRebalanceResolverTest tests', function() {
+
+  const USDC_ADDRESS = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
+  const USDC_BIG_HOLDER_ADDRESS = '0xe7804c37c13166ff0b37f5ae0bb07a3aebb6e245';
+  const TETU_BOOSTED_USDC_REBALANCER = '0x9756549a334bd48423457d057e8edbfaf2104b16';
+
+  const DEFAULT_AMOUNT = '1000';
+
+  async function deployContracts() {
+    const [signer, other] = await ethers.getSigners();
+
+    const BoostedPoolsRebalanceResolverFact = await ethers.getContractFactory('BoostedPoolsRebalanceResolver');
+    const resolver = await upgrades.deployProxy(
+      BoostedPoolsRebalanceResolverFact,
+      [rebalancers],
+    ) as BoostedPoolsRebalanceResolver;
+
+    const balRebalancer = await ethers.getContractAt('ILinearPoolRebalancer', TETU_BOOSTED_USDC_REBALANCER);
+    const poolAddress = await balRebalancer.getPool();
+    const pool = await ethers.getContractAt('IBalancerBoostedAaveStablePool', poolAddress);
+    const poolId = await pool.getPoolId();
+    const vault = await ethers.getContractAt('IBVault', await pool.getVault());
+
+
+    return { resolver, vault, poolId, signer };
+  }
+
+  describe('Smoke tests', function() {
+    it('Owner properly set', async function() {
+      const { resolver, signer } = await loadFixture(deployContracts);
+      expect(signer.address).is.eq(await resolver.owner());
+    });
+
+    it('updateRebalancers test', async function() {
+      const { resolver, signer } = await loadFixture(deployContracts);
+      await resolver.updateRebalancers([TETU_BOOSTED_USDC_REBALANCER]);
+      const { canExec } = await resolver.checker();
+      expect(canExec).is.eq(true);
+    });
+
+    it.skip('execution test', async function() {
+      const { resolver, signer } = await loadFixture(deployContracts);
+
+
+      const data = await resolver.checker();
+
+      expect(data.canExec).eq(true);
+      const callData = BoostedPoolsRebalanceResolver__factory.createInterface()
+        .decodeFunctionData('rebalance', data.execPayload);
+
+      const balancerRebalancer = callData.balancerRebalancer;
+      const amount = callData.amount;
+      const extra = callData.extra;
+
+      console.log('balancerRebalancer', balancerRebalancer);
+      console.log('amount', amount.toString());
+      console.log('extra', extra);
+
+      await resolver.changeOperatorStatus(signer.address, true);
+      await resolver.rebalance(balancerRebalancer, amount, extra);
+    });
+
+  });
+});
