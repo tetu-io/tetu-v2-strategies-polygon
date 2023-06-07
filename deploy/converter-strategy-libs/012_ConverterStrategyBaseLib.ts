82bd3655c08c5b0d4e4c0b7c8c8e28dd1fc81701
Alien Deployer <m@a17.name>
2023-06-07 14:31:22 +0300

    fix and skip deploy scripts

--- deploy/converter-strategy-libs/012_ConverterStrategyBaseLib.ts
+++ deploy/converter-strategy-libs/012_ConverterStrategyBaseLib.ts
@@ -3,8 +3,12 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { hardhatDeploy } from '../../deploy_constants/deploy-helpers';
 
 const func: DeployFunction = async function(hre: HardhatRuntimeEnvironment) {
+  const { deployments } = hre;
   await hardhatDeploy(hre, 'ConverterStrategyBaseLib', true);
-  await hardhatDeploy(hre, 'ConverterStrategyBaseLib2', true);
+  await hardhatDeploy(hre, 'ConverterStrategyBaseLib2', true, {
+    StrategyLib: (await deployments.get('StrategyLib')).address,
+  });
 };
 export default func;
 func.tags = ['ConverterStrategyBaseLib', 'ConverterStrategyBaseLib2'];
+func.dependencies = ['StrategyLib',]
