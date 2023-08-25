6f0a5e1466b3bf9b46897da269f0a6f8eadfeec8
dv <dvpublic0@gmail.com>
2023-08-25 19:28:14 +0700

    Bump version of ConverterStrategyBase

--- contracts/strategies/ConverterStrategyBase.sol
+++ contracts/strategies/ConverterStrategyBase.sol
@@ -57,7 +57,7 @@ abstract contract ConverterStrategyBase is ITetuConverterCallback, DepositorBase
   //region -------------------------------------------------------- CONSTANTS
 
   /// @dev Version of this contract. Adjust manually on each code modification.
-  string public constant CONVERTER_STRATEGY_BASE_VERSION = "2.0.1";
+  string public constant CONVERTER_STRATEGY_BASE_VERSION = "2.0.2";
 
   /// @notice 1% gap to cover possible liquidation inefficiency
   /// @dev We assume that: conversion-result-calculated-by-prices - liquidation-result <= the-gap
