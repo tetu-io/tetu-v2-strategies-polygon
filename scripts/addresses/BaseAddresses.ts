c970f54f477f39fb7543cd4c7319e2a75c8b4daf
dv <dvpublic0@gmail.com>
2023-12-20 11:31:30 +0700

    Move checkBase to analysis folder. Add CheckPolygon

--- scripts/addresses/BaseAddresses.ts
+++ scripts/addresses/BaseAddresses.ts
@@ -59,4 +59,10 @@ export class BaseAddresses {
 
 //endregion ----------------------------------------------------- PancakeSwap
 
+//region ----------------------------------------------------- Aggregators
+
+    public static AGG_ONEINCH_V5 = '0x1111111254EEB25477B68fb85Ed929f73A960582'.toLowerCase()
+
+//endregion ----------------------------------------------------- Aggregators
+
 }
--- test/analysis/CheckPolygon.ts
+++ test/analysis/CheckPolygon.ts
@@ -1,4 +1,4 @@
-import {BASE_NETWORK_ID, HardhatUtils} from "../baseUT/utils/HardhatUtils";
+import {BASE_NETWORK_ID, HardhatUtils, POLYGON_NETWORK_ID} from "../baseUT/utils/HardhatUtils";
 import {TimeUtils} from "../../scripts/utils/TimeUtils";
 import {DeployerUtilsLocal} from "../../scripts/utils/DeployerUtilsLocal";
 import {
@@ -13,24 +13,29 @@ import {IStateNum, StateUtilsNum} from '../baseUT/utils/StateUtilsNum';
 import {CaptureEvents, IEventsSet} from '../baseUT/strategies/CaptureEvents';
 import fs from 'fs';
 import {ENTRY_TO_POOL_IS_ALLOWED} from '../baseUT/AppConstants';
-import {BaseAddresses} from '../../scripts/addresses/BaseAddresses';
 import {buildEntryData1} from '../baseUT/utils/EntryDataUtils';
 import {AggregatorUtils} from '../baseUT/utils/AggregatorUtils';
 import {PackedData} from '../baseUT/utils/PackedData';
 import {MockHelper} from '../baseUT/helpers/MockHelper';
 import {ethers} from "hardhat";
 import {Misc} from "../../scripts/utils/Misc";
+import {MaticAddresses} from "../../scripts/addresses/MaticAddresses";
+
+async function injectStrategy() {
+  // await InjectUtils.injectTetuConverterBeforeAnyTest(signer);
+  // await InjectUtils.injectStrategy(signer, STRATEGY, "UniswapV3ConverterStrategy");
+}
 
 describe("Check actions on base @skip-on-coverage", () => {
-  const BLOCK = 8073170;
-  const STRATEGY = "0xAA43e2cc199DC946b3D528c6E00ebb3F4CC2fC0e";
-  const CONTROLLER = "0x255707B70BF90aa112006E1b07B9AeA6De021424";
+  const BLOCK = 51312800;
+  const STRATEGY = "0xA8105284aA9C9A20A2081EEE1ceeF03d9719A5AD";
+  const CONTROLLER = "0x33b27e0a2506a4a2fbc213a01c51d0451745343a";
   const SENDER = "0xbbbbb8c4364ec2ce52c59d2ed3e56f307e529a94";
-  const SPLITTER = "0xA01ac87f8Fc03FA2c497beFB24C74D538958DAbA";
+  // const SPLITTER = "";
 
   let snapshotBefore: string;
   beforeEach(async function () {
-    await HardhatUtils.setupBeforeTest(BASE_NETWORK_ID, BLOCK);
+    await HardhatUtils.setupBeforeTest(POLYGON_NETWORK_ID, BLOCK);
     snapshotBefore = await TimeUtils.snapshot();
 
     // we need to display full objects, so we use util.inspect, see
@@ -40,11 +45,10 @@ describe("Check actions on base @skip-on-coverage", () => {
 
   afterEach(async function () {
     await TimeUtils.rollback(snapshotBefore);
-    await HardhatUtils.restoreBlockFromEnv();
   });
 
   it("try to rebalance", async () => {
-    const pathOut = "./tmp/checkBase-rebalance.csv";
+    const pathOut = "./tmp/checkPolygon-rebalance.csv";
     const signer = await DeployerUtilsLocal.impersonate(SENDER);
     const strategyAsOperator = IRebalancingV2Strategy__factory.connect(STRATEGY, signer);
     const converterStrategyBase = ConverterStrategyBase__factory.connect(STRATEGY, signer);
@@ -62,20 +66,23 @@ describe("Check actions on base @skip-on-coverage", () => {
     const saver = async (title: string, e?: IEventsSet) => {
       const state = await StateUtilsNum.getState(signer, signer, converterStrategyBase, vault, title, {eventsSet: e});
       states.push(state);
-      StateUtilsNum.saveListStatesToCSVColumns(pathOut, states, {mainAssetSymbol: BaseAddresses.USDC_TOKEN});
+      StateUtilsNum.saveListStatesToCSVColumns(pathOut, states, {mainAssetSymbol: MaticAddresses.USDC_TOKEN});
     };
 
-    // await InjectUtils.injectStrategy(signer, STRATEGY, "UniswapV3ConverterStrategy");
+    await injectStrategy();
 
     await saver("b");
-    const eventsSet = await CaptureEvents.makeRebalanceNoSwap(strategyAsOperator);
-    await saver("a", eventsSet);
+    if (await strategyAsOperator.needRebalance()) {
+      const eventsSet = await CaptureEvents.makeRebalanceNoSwap(strategyAsOperator);
+      await saver("a", eventsSet);
+    } else {
+      console.log("no rebalance needed");
+    }
 
-    await HardhatUtils.restoreBlockFromEnv();
   });
 
   it("try withdrawByAgg", async () => {
-    const pathOut = "./tmp/checkBase-withdrawByAgg.csv";
+    const pathOut = "./tmp/checkPolygon-withdrawByAgg.csv";
     const signer = await DeployerUtilsLocal.impersonate(SENDER);
 
     const strategyAsOperator = IRebalancingV2Strategy__factory.connect(STRATEGY, signer);
@@ -94,14 +101,13 @@ describe("Check actions on base @skip-on-coverage", () => {
     const saver = async (title: string, e?: IEventsSet): Promise<IStateNum> => {
       const state = await StateUtilsNum.getState(signer, signer, converterStrategyBase, vault, title, {eventsSet: e});
       states.push(state);
-      StateUtilsNum.saveListStatesToCSVColumns(pathOut, states, {mainAssetSymbol: BaseAddresses.USDbC_TOKEN});
+      StateUtilsNum.saveListStatesToCSVColumns(pathOut, states, {mainAssetSymbol: MaticAddresses.USDC_TOKEN});
       return states[states.length - 1];
     };
 
-    await InjectUtils.injectStrategy(signer, STRATEGY, "UniswapV3ConverterStrategy");
-    // await PairBasedStrategyPrepareStateUtils.injectTetuConverter(signer);
+    await injectStrategy();
 
-    const aggregator = "0x1111111254EEB25477B68fb85Ed929f73A960582";
+    const aggregator = MaticAddresses.AGG_ONEINCH_V5;
 
     const state = await PackedData.getDefaultState(strategyAsOperator);
     const reader = await MockHelper.createPairBasedStrategyReader(signer);
@@ -138,13 +144,13 @@ describe("Check actions on base @skip-on-coverage", () => {
     console.log("unfoldBorrows.withdrawByAggStep.FINISH --------------------------------");
     const stateAfter = await saver("a", eventsSet);
     console.log("stateAfter", stateAfter);
-    await HardhatUtils.restoreBlockFromEnv();
   });
 
   it("doHardWork", async () => {
-    const pathOut = "./tmp/checkBase-hardwork.csv";
+    const pathOut = "./tmp/checkPolygon-hardwork.csv";
     const states: IStateNum[] = [];
 
+    console.log("-------------------------------- prepare saver");
     if (fs.existsSync(pathOut)) {
       fs.rmSync(pathOut);
     }
@@ -152,14 +158,15 @@ describe("Check actions on base @skip-on-coverage", () => {
     const saver = async (title: string, e?: IEventsSet) => {
       const state = await StateUtilsNum.getState(signer, signer, converterStrategyBase, vault, title, {eventsSet: e});
       states.push(state);
-      StateUtilsNum.saveListStatesToCSVColumns(pathOut, states, {mainAssetSymbol: BaseAddresses.USDbC_TOKEN});
+      StateUtilsNum.saveListStatesToCSVColumns(pathOut, states, {mainAssetSymbol: MaticAddresses.USDC_TOKEN});
     };
 
     // const signer = await DeployerUtilsLocal.impersonate(SENDER);
     const signer = (await ethers.getSigners())[0];
 
-    const operator = await Misc.impersonate((await ControllerV2__factory.connect(CONTROLLER, signer).operatorsList())[0]);
 
+    console.log("-------------------------------- get addresses");
+    const operator = await Misc.impersonate((await ControllerV2__factory.connect(CONTROLLER, signer).operatorsList())[0]);
     const strategyAsOperator = IRebalancingV2Strategy__factory.connect(STRATEGY, operator);
     const converterStrategyBase = ConverterStrategyBase__factory.connect(STRATEGY, operator);
     const vault = TetuVaultV2__factory.connect(
@@ -168,9 +175,12 @@ describe("Check actions on base @skip-on-coverage", () => {
     );
     const splitter = await vault.splitter();
 
-    // await InjectUtils.injectTetuConverterBeforeAnyTest(signer);
-    // await InjectUtils.injectStrategy(signer, STRATEGY, "UniswapV3ConverterStrategy");
 
+    console.log("-------------------------------- inject strategy");
+    await injectStrategy();
+
+
+    console.log("-------------------------------- make hardwork");
     await saver("b");
     const strategyAsSplitter = converterStrategyBase.connect(await DeployerUtilsLocal.impersonate(splitter));
     if (await strategyAsOperator.needRebalance()) {
